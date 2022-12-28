91ca0ce34dd6307a0ddab8c08c1e912ad224bb82
isakgranqvist2021 <isakwebdev@gmail.com>
2022-12-28 02:48:06 +0100

    Add persistance class

---
+++ Services/Persistance.cs
@@ -0,0 +1,19 @@
+namespace RecGen.Services;
+
+public class Persistance
+{
+    public void Read()
+    {
+
+    }
+
+    public void Insert()
+    {
+
+    }
+
+    public void Delete()
+    {
+
+    }
+}
